e35bb716311f6eab4773ddb0a735e87bc68d24bf
ManieBhuju <52810029+ManieBhuju@users.noreply.github.com>
2020-04-24 12:36:02 +0545

    Added geolocation key
    added a key for google map and pasted a code of sample google map(btw its not working need to fix it)

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyBSOHPLRS7eBfU-xXlCG-GUKJYKLO5-O98")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
