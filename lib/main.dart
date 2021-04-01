fe24e49d1dea2d762a218d6646fa3dc93c20734f
Manie Bhuju <manibhuju@gmail.com>
2021-04-01 19:53:51 +0545

    finally firebase needs to be initilized at the beginning

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:track_the_bus/screens/wrapper.dart';
 import 'package:provider/provider.dart';
 import 'package:track_the_bus/services/auth.dart';
 import 'package:track_the_bus/models/customuser.dart';
 
-void main() => runApp(TrackTheBus());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(TrackTheBus());
+}
 
 class TrackTheBus extends StatelessWidget {
   @override
